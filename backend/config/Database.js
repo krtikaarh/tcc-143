53160fcd6c704e3b49d912eaee8c18b041fc4bae
Kartika Rahmi <kartikarahmi7@gmail.com>
2025-04-20 18:20:57 +0700

    edit

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("RECOVER_YOUR_DATA", "root", "", {
+const db = new Sequelize("RECOVER_YOUR_DATA", "root", "root1234", {
   host: "34.60.175.254",
   dialect: "mysql",
 });
