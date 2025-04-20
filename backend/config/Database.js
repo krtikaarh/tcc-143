9ff6e304f501792d612eef4fb1e1404f910b50f3
Kartika Rahmi <kartikarahmi7@gmail.com>
2025-04-20 15:36:34 +0700

    edit

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,12 +1,8 @@
 import { Sequelize } from "sequelize";
 
-// Gunakan variabel lingkungan yang sudah diset di Cloud Run
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const db = new Sequelize("RECOVER_YOUR_DATA", "root", "", {
+  host: "34.60.175.254",
   dialect: "mysql",
-  dialectOptions: {
-    socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,  // Menggunakan socket path Cloud SQL
-  },
-  logging: false,
 });
 
 export default db;
--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,14 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
-import NoteRoutes from "./routes/UserRoute.js";
-import db from "./config/Database.js";
-import Note from "./models/UserModel.js";
+import route from "./routes/route.js";
 
 const app = express();
+const port = process.env.PORT | 5000;
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use("/api", NoteRoutes);
+app.use(express.json());
+app.use(route);
 
-const PORT = process.env.PORT || 5000;
-console.log("PORT:", PORT);
-
-const startServer = async () => {
-    try {
-        console.log("Mencoba koneksi ke database...");
-        await db.authenticate();
-        console.log("Database connected...");
-        await db.sync(); // Sinkronisasi model ke database
-        app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
-    } catch (error) {
-        console.error("Connection error:", error.message);
-        console.error(error); // biar tahu trace lengkap
-
-    }
-};
-
-startServer();
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
