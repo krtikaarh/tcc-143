69523d33cd5452778d713a6cbc509ee29e8cf98c
Kartika Rahmi <kartikarahmi7@gmail.com>
2025-04-20 16:46:44 +0700

    edit

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import cors from "cors";
 import route from './routes/UserRoute.js';
+import db from './config/Database.js';
 
 const app = express();
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(route);
+
+db.authenticate()
+  .then(() => console.log("✅ Database connected"))
+  .catch((err) => console.error("❌ Database connection error:", err));
 
 app.get('/', (req, res) => {
     res.send('API is running ✅');
