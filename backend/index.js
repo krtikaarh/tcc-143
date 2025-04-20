e6b965945a3f7bee2ac476426ccce9148609cb95
Kartika Rahmi <kartikarahmi7@gmail.com>
2025-04-20 15:41:37 +0700

    edit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import route from "./routes/route.js";
+import route from "UserRoutes.js";
 
 const app = express();
 const port = process.env.PORT | 5000;
